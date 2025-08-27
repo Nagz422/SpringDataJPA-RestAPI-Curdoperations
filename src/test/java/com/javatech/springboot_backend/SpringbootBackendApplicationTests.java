ef4af6286f6f5e581483ad561c8ba2c6cfc7e6fe
Nagz422 <nag.ch922@gmail.com>
2025-08-27 15:45:49 +0530

    Update SpringbootBackendApplicationTests.java

--- src/test/java/com/javatech/springboot_backend/SpringbootBackendApplicationTests.java
+++ src/test/java/com/javatech/springboot_backend/SpringbootBackendApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class SpringbootBackendApplicationTests {
 
 	@Test
